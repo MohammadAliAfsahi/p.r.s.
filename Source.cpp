265ba497c18dbdd0814d4bbf662a5a509c26312c
MohammadAliAfsahi <35253602+MohammadAliAfsahi@users.noreply.github.com>
2018-01-10 09:59:03 +0330

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -7,6 +7,7 @@ typedef struct information
 
 };
 void show_list_of_teams();
+void print_situation_game(char *token,char *name);
 int main()
 {
 	FILE *saved_game;
@@ -16,6 +17,54 @@ int main()
 		if (getchar() == 10)
 		{
 			show_list_of_teams();
+			FILE *fp = fopen("C:\\Users\\Mohammad Ali\\Documents\\Visual Studio 2017\\Projects\\PROJECT\\PROJECT\\Teams\\teams.txt", "r");
+			char *token;
+			int i;
+			if (fp != NULL)
+			{
+				char line[1000];
+				while (fgets(line, 1000, fp) != NULL)
+				{
+					for (i = 0; i<6; i++)
+					{
+						switch (i)
+						{
+						case 0:
+							char *name;
+							name = strtok(line, ",");
+							//printf("%s\t", token);
+							//break;
+						case 1:
+							token = strtok(NULL, ",");
+							print_situation_game(token,name);
+							break;
+						case 2:
+							token = strtok(NULL, ",");
+							
+							break;
+						case 3:
+							token = strtok(NULL, ",");
+							
+							break;
+						case 4:
+							token = strtok(NULL, ",");
+							
+							break;
+						case 5:
+							token = strtok(NULL, ",");
+							break;
+						default:
+							break;
+						}
+					}
+					puts("");
+				}
+				system("pause");
+				fclose(fp);
+			}
+			else {
+				perror("C:\\Users\\Mohammad Ali\\Documents\\Visual Studio 2017\\Projects\\PROJECT\\PROJECT\\Teams\\teams.txt");
+			}
 		}
 	}
 	else
@@ -29,14 +78,26 @@ void show_list_of_teams()
 {
 	int step=0;
 	char name[10000];
-	FILE *name_game = fopen("c:\\Users\\eli\\Desktop\\teams.txt", "r");
+	FILE *name_game = fopen("C:\\Users\\Mohammad Ali\\Documents\\Visual Studio 2017\\Projects\\PROJECT\\PROJECT\\Teams\\teams.txt", "r");
 		int i = 0;
 		for (i = 0; i<32; i++) 
 		{
 			fgets(name, 100000000, name_game);
 			strtok(name, ",");
-			printf("%d. %s\n",++step, name);
+			printf("%d. %s\t",++step, name);
+			if (step%8==0)
+			{
+				puts(" ");
+			}
 		}
 	
 		system("pause");
+}
+void print_situation_game(char *token,char *name)
+{
+	
+	if (*token == 'D')
+	{
+		printf("%s\n",name);
+	}
 }
